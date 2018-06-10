3864679b270dd09e9eb58d5e5a032cdfd426025d
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-10 14:23:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     binding.pry
-    @song.genres = params[:genres]
+    @song.genre_ids = params[:genres]
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
     flash[:message] = "Successfully updated song."
